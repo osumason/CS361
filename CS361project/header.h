d14b156f0d1e07a6cafbe685ce4923ca1a9f1099
Masonantram <antramm@my.lanecc.edu>
2026-02-04 21:14:37 -0800

    basic story, creates and displays empty map

---
+++ CS361project/header.h
@@ -0,0 +1,15 @@
+//
+// Created by mason on 2/4/2026.
+//
+
+#ifndef STORY_H
+#define STORY_H
+#include <vector>
+
+void storytime();
+void sizemap(std::vector<std::vector<char>>&map, int size);
+void makemap(std::vector<std::vector<char>>&map, int size);
+void displaymap(std::vector<std::vector<char>>&map, int size);
+
+
+#endif //STORY_H
---
+++ CS361project/story.cpp
@@ -0,0 +1,23 @@
+//
+// Created by mason on 2/4/2026.
+//
+
+#include <iostream>
+
+#include "header.h"
+
+
+
+void storytime()
+{
+
+    std::cout << "In an age of forgotten gods and crumbling empires, the world of Oakhaven teetered on the brink of "
+                 "Eternal Silence. \nThe Great Void began to swallow the stars, and the oracles wept, for no king, no mage, "
+                 "and no warrior of old could \nhalt the darkness. But the ancient scrolls spoke of a different kind of "
+                 "power. Not a title, not a lineage, but a Name. \n \n";
+
+
+    std::cout << "\t\"When the sky bleeds crimson and the mountains bow, there shall arise a traveler of no renown but "
+                 "infinite will. \n \tHe shall be the anchor in the storm. He shall be the blade in the dark. "
+                 "He shall be... JEFF.\" \n" ;
+}
